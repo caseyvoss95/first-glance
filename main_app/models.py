f49b952c326c4dbd264434b3ffb10c91eed1d1cb
caseyvoss95 <casey.vos@bu.edu>
2022-10-21 16:01:35 -0400

    photo model added

--- main_app/models.py
+++ main_app/models.py
@@ -3,3 +3,10 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=50)
     imgPath =  models.CharField(max_length=100)
+
+class Photo(models.Model):
+    url = models.CharField(max_length=200)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Photo for person_id {self.person_id} @{self.url}"
