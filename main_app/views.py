7545266e724ed7b77531af7ac72d99c1f387fed5
caseyvoss95 <casey.vos@bu.edu>
2022-10-25 23:05:41 -0400

    debugged signup

--- main_app/views.py
+++ main_app/views.py
@@ -84,7 +84,7 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('index')
+            return redirect('group_view')
         else:
             error_message = 'Invalid sign up - try again'
     form = UserCreationForm()
