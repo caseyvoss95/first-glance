2938075fab65708591aa7bba4a6ee94d7ba00f47
caseyvoss95 <casey.vos@bu.edu>
2022-10-25 23:15:50 -0400

    added more photo and name assets

--- main_app/views.py
+++ main_app/views.py
@@ -8,7 +8,7 @@ from .models import Person, Question
 from .forms import QuestionForm
 
 #assets
-names = ['Dorian', 'Amos', 'Flynn', 'Kori', 'Ellison', 'Harper', 'Wyatt', 'Asher']
+names = ['Dorian', 'Amos', 'Flynn', 'Kori', 'Ellison', 'Harper', 'Wyatt', 'Asher', 'River', 'Logan', 'Cameron', 'Rowan', 'Peyon', 'Ryan', 'Hayden']
 
 images = [
     'https://images.generated.photos/RWgvMrrBsLFd8l-n8DRUpNswZF9A5qW2cN0qGUsnMps/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NzEyNTAzLmpwZw.jpg',
@@ -19,6 +19,17 @@ images = [
     'https://images.generated.photos/6AZE02WGr1kJvdJkpGndYH2G3LPcoZxIohIxL_ZrelY/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MjU5NTU1LmpwZw.jpg',
     'https://images.generated.photos/RgdJ6UxHJyqOejDLu-QsA0Cds3eEgRnZNXSwFSz9pKQ/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NDE3NjEyLmpwZw.jpg',
     'https://images.generated.photos/isEv8ypmyC47Dzn1WZLaLFGNQ__Zv-ok3SbXPRBIsqw/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/OTgxNjU0LmpwZw.jpg',
+    'https://images.generated.photos/uIi20RXpJRth_wL4X328Lrhk0-dPbketmewWxpYheDM/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NDcyMDIzLmpwZw.jpg',
+    'https://images.generated.photos/0kTfTK1NMd1kaMxX7LD8lou4M22jWq0SWn2je8sSn8E/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MTc3MTcyLmpwZw.jpg',
+    'https://images.generated.photos/yZe4-qr0QKM1djPOhY9TfynsPSNdAWX7TzDpiXDFWas/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MjU0ODE0LmpwZw.jpg',
+    'https://images.generated.photos/WwPo1RYmEG_jGgYgs55Pe6_ro0KUOItDnBP5Q4xixxc/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MTg4ODk0LmpwZw.jpg',
+    'https://images.generated.photos/beOlEbcLFvwyyF4NcWl41CHzOUM_j1JBmiVv9rfXNdo/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MzAzMzAyLmpwZw.jpg',
+    'https://images.generated.photos/hcOSbwasMacRvMSLBZU7vPR1j6OCHmO-2pz1RY2J2KE/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/OTIyMjM3LmpwZw.jpg',
+    'https://images.generated.photos/sg6_f1W-Gavm9iLSKNTDI6LXE9HRe12fid1hAp_m7rU/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/ODc1ODM4LmpwZw.jpg',
+    'https://images.generated.photos/BMTF3WJvA_XfwfVO_KlfMrDNk1sTsGSZoDSigC-TiqQ/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NzYyOTkzLmpwZw.jpg',
+    'https://images.generated.photos/s3c2hmlLpJ4re4mrEhh31trI6J80CZ6sX8HCPkd4TVo/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/OTQ4OTM0LmpwZw.jpg',
+    'https://images.generated.photos/Sl38uFmDvGDzoiXW3cdpfvx4gIvQAzftxE2hqi0LGwg/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MzI3MjUzLmpwZw.jpg',
+    'https://images.generated.photos/Cd-Jnz68aQn3IFk73HXJjFo7e69mM9MS3B8N3b_LaGs/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/ODQwMDQ2LmpwZw.jpg'
 ]
 
 @login_required
@@ -73,9 +84,6 @@ def results(request):
             score += 1
         questionId += 1
     return render(request, 'results.html', {'score': score})
-
-    
-
 
 def signup(request):
     error_message = ''
